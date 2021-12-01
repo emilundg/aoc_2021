eb34b9b8314e752d547a82489b2b522852489874
emilundg <lundgrenemil@hotmail.com>
2021-12-01 16:00:37 +0100

    WORKING solution

--- day01/index.js
+++ day01/index.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
 
+function inputDataLinesIntegers(filename="input.txt") {
+    return fs.readFileSync(filename).toString().trim().split("\n").map((x)=>parseInt(x))
+}
+
 const determineIncrease = (data) => {
-    let increasedCount = 0
+    let increasedCount = 0;
     let i = 1;
     do {
         if (data[i-1] < data[i]) {
@@ -9,14 +13,39 @@ const determineIncrease = (data) => {
         }
         i++
     } while (i < data.length); 
-    console.log(increasedCount)
+    return increasedCount;
 }
 
-fs.readFile('./testdata.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    const lines = data.split(/\r?\n/);
-    determineIncrease(lines)
-})
+function getSolutionPart1() {
+    return determineIncrease(inputDataLinesIntegers())
+}
+
+const determineWindowIncrease = (data) => {
+    let i = 1;
+    let prevSetValue = data[0] + data[1] + data[2];
+    let increasedCount = 0;
+    do {
+        const setValue = data[i] + data[i+1] + data[i+2]
+        if (prevSetValue < setValue) {
+            increasedCount+=1
+        }
+        prevSetValue = setValue;
+        i++;
+    } while (i < data.length - 2);
+    return increasedCount;
+}
+
+function getSolutionPart2() {
+    return determineWindowIncrease(inputDataLinesIntegers()) 
+}
+
+const part = process.env.part || "part1"
+
+if (part === "part1")
+    console.log(getSolutionPart1())
+else
+    console.log(getSolutionPart2())
+
+module.exports = {
+    getSolutionPart1, getSolutionPart2, inputDataLinesIntegers
+}
