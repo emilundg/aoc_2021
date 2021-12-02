5e4448b5620372e6d6cd7ac14f320f8e855188e3
emilundg <lundgrenemil@hotmail.com>
2021-12-02 10:40:27 +0100

    REMOVE parenthesis

--- day02/index.js
+++ day02/index.js
@@ -14,9 +14,9 @@ const determineDirection = (data, withAim) => {
             horizontalPosition += num
             if (withAim) depth += (aim * num)
         } else if (direction === 'd') {
-            withAim ? (aim += num) : (depth += num)
+            withAim ? aim += num : depth += num
         } else if (direction === 'u') {
-            withAim ? (aim -= num) : (depth -=num)
+            withAim ? aim -= num : depth -=num
         }
        i++ 
     } while (i < data.length);
