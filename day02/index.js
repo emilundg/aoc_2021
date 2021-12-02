17169c3e937775b4310c5b82e36fb3356f73861c
emilundg <lundgrenemil@hotmail.com>
2021-12-02 08:43:24 +0100

    WORKING solution p1

--- day02/index.js
+++ day02/index.js
@@ -4,8 +4,27 @@ function inputDataLinesIntegers(filename="input.txt") {
     return fs.readFileSync(filename).toString().trim().split("\n")
 }
 
+let horizontalPosition = 0;
+let depth = 0;
+const determineDirection = (data) => {
+    let i = 0;
+    do {
+        const currentData = data[i]
+        const direction = currentData[0]
+        if (direction === 'f') {
+            horizontalPosition += parseInt(data[i].split(" ")[1])
+        } else if (direction === 'd') {
+            depth += parseInt(data[i].split(" ")[1])
+        } else if (direction === 'u') {
+            depth -= parseInt(data[i].split(" ")[1])
+        }
+       i++ 
+    } while (i < data.length);
+}
+
 function getSolutionPart1() {
-    return inputDataLinesIntegers()
+    determineDirection(inputDataLinesIntegers())
+    return horizontalPosition * depth
 }
 
 function getSolutionPart2() {
