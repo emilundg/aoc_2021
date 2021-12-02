cc96fb7148a7f1e5ba15c5fd6ce86fa2cca72972
emilundg <lundgrenemil@hotmail.com>
2021-12-02 08:30:43 +0100

    FIX reader

--- day02/index.js
+++ day02/index.js
@@ -1,14 +1,24 @@
+const fs = require('fs')
+
+function inputDataLinesIntegers(filename="input.txt") {
+    return fs.readFileSync(filename).toString().trim().split("\n")
+}
+
 function getSolutionPart1() {
-    return 1337;
+    return inputDataLinesIntegers()
 }
 
 function getSolutionPart2() {
-    return 42;
+    return inputDataLinesIntegers() 
 }
 
-const part = process.env.part || "part1";
+const part = process.env.part || "part1"
 
 if (part === "part1")
-    console.log(getSolutionPart1());
+    console.log(getSolutionPart1())
 else
-    console.log(getSolutionPart2());
+    console.log(getSolutionPart2())
+
+module.exports = {
+    getSolutionPart1, getSolutionPart2, inputDataLinesIntegers
+}
