a6dddfe4c969a77a67bb1a1c953d1991db514deb
emilundg <lundgrenemil@hotmail.com>
2021-12-04 19:00:40 +0100

    REWRITE p1

--- day03/index.js
+++ day03/index.js
@@ -2,30 +2,27 @@ const fs = require('fs');
 const inputDataLinesIntegers = (filename="input.txt") => fs.readFileSync(filename).toString().trim().split("\n");
 
 const determinePowerUsage = (data) => {
-    const dataLength = data.length;
-    let i = 0, j = 0, indexArray = [0,0,0,0,0,0,0,0,0,0,0,0], gammaRateArray = [], epsilonRateArray = []; 
+    let i = 0, gammaRate = "", epsilonRate = "";
     do {
+        let j = 0, ones = 0, zeroes = 0;
         do {
-            if (data[i][j] === "1") indexArray[j]+=1;
-            if (indexArray[j] > dataLength / 2) {
-                gammaRateArray[j] = "1"
-                epsilonRateArray[j] = "0"
-            } else {
-                gammaRateArray[j] = "0";
-                epsilonRateArray[j] = "1";
-            }
+            data[j][i] === "1" ? ones+=1 : zeroes+=1;
             j++;
-        } while (j < data[i].length);
-        j = 0;
+        } while (j < data.length);
+        if (ones > zeroes) {
+            gammaRate+=1;
+            epsilonRate+=0;
+        } else {
+            gammaRate+=0;
+            epsilonRate+=1;
+        }
         i++; 
-    } while (i < dataLength);
-    const gammaBinary = gammaRateArray.join("");
-    const epsilonBinary = epsilonRateArray.join("");
-    return parseInt(gammaBinary, 2) * parseInt(epsilonBinary, 2);
+    } while (i < 12);
+    return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2); 
 }
 
 const getSolutionPart1 = () => determinePowerUsage(inputDataLinesIntegers());
-const getSolutionPart2 = () => determinePowerUsage(inputDataLinesIntegers());
+const getSolutionPart2 = () => determineLifeSupport(inputDataLinesIntegers());
 
 const part = process.env.part || "part1";
 part === "part1" ? console.log(getSolutionPart1()) : console.log(getSolutionPart2());
